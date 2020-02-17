e5908aa7733203b801dba06af64dac02f4582ca3
MC <mc69.dashboard@gmail.com>
2020-02-17 16:49:39 +0100

    add upload config. to DocServlet

--- src/cda/afip/servlets/DocServlet.java
+++ src/cda/afip/servlets/DocServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +22,10 @@ import cda.afip.model.UserManager;
  * Servlet implementation class DocServlet
  */
 @WebServlet("/docs")
+
+@MultipartConfig(fileSizeThreshold = 1024 * 1024,
+maxFileSize = 1024 * 1024 * 5, 
+maxRequestSize = 1024 * 1024 * 5 * 5)
 public class DocServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static Logger logger = Logger.getLogger(DocServlet.class);
